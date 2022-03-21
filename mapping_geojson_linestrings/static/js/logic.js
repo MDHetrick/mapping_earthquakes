514483056b928836aa6cf3bfe3e94e5a2e593cfd
MeLea Hetrick <meletrick@gmail.com>
2022-03-21 13:27:03 -0500

    M

--- mapping_geojson_linestrings/static/js/logic.js
+++ mapping_geojson_linestrings/static/js/logic.js
@@ -30,12 +30,25 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let torontoData = "https://raw.githubusercontent.com/MDHetrick/mapping_earthquakes/main/torontoRoutes.json";
-console.log(toronto)
+console.log(torontoData)
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+L.geoJSON(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
+  }
+})
+.addTo(map);
 });
+
