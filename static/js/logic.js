5103dd4211578e50979c66e562b9ce4852185258
MeLea Hetrick <meletrick@gmail.com>
2022-03-21 11:28:19 -0500

    20220321

---
+++ static/js/logic.js
@@ -0,0 +1,6 @@
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/MDHetrick/mapping_earthquakes/main/majorAirports.json";
+
