babfd36b018677de06d630cd1ade40121a313509
MeLea Hetrick <Hetrick.Melea@mayo.edu>
2022-03-24 10:55:40 -0500

    challenge

--- earthquake_challenge/static/js/challenge_logic.js
+++ earthquake_challenge/static/js/challenge_logic.js
@@ -77,8 +77,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     
   }
 
-
-
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 5) {
@@ -98,8 +96,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return "#98ee00";
   }
-
-    
+  
   // This function determines the radius of the earthquake marker based on its magnitude.
   // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
   function getRadius(magnitude) {
@@ -108,8 +105,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return magnitude * 4;
   }
-
-
 
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
@@ -175,53 +170,60 @@ legend.onAdd = function() {
   });
 });
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
 // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
 
 // 4. Use the same style as the earthquake data.
-
-function getColor(magnitude) {
+function fillColor(magnitude) {
   if (magnitude > 6) {
-    return "#ee0000"
+    return "#FE3003"
   }
   if (magnitude > 5) {
-    return "#ea2c2c";
+    return "#FE9B03";
+  }
+    return "#FEFA03";
   }
 
-    return "#ea822c";
-  }
-
+  function getColor(magnitude) {
+    if (magnitude > 6) {
+      return "#641E16"
+    }
+    if (magnitude > 5) {
+      return "#6E2C00";
+    }
+      return "#7D6608";
+    }
 
   function getRadius(magnitude) {
-    // if (magnitude === 0) {
-    //   return 1;
-    // }
-    return magnitude * 4;
-  }  
+    if (magnitude > 6) {
+      return magnitude * 12
+    }
+    if (magnitude > 5) {
+      return magnitude * 10;
+    }
+      return magnitude * 8;
+    }
+  
   function styleInfo(feature) {
     return {
       opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "#000000",
+      fillOpacity: 0.7,
+      fillColor: fillColor(feature.properties.mag),
+      color: getColor(feature.properties.mag),
       radius: getRadius(feature.properties.mag),
       stroke: true,
-      weight: 0.5
+      weight: 0.9
     };
   };
-L.geoJson(data, {
-  // We turn each feature into a circleMarker on the map.
-  pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng);
-    },
-  // We set the style for each circleMarker using our styleInfo function.
-style: styleInfo,
- // We create a popup for each circleMarker to display the magnitude and location of the earthquake
- //  after the marker has been created and styled.
- onEachFeature: function(feature, layer) {
-  layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+
+  style: styleInfo,
+
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
 }
-}).addTo(majorEarthquakes); })
+}).addTo(majorEarthquakes);})
